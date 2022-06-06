cac69ae14be7346e71b8a526bba65bfc64ed3ce2
hrwx <himanshuwarekar@yahoo.com>
2022-06-06 10:39:36 +0530

    chore: docstrings for api endpoints

--- shortlink/routes.py
+++ shortlink/routes.py
@@ -14,6 +14,17 @@ from shortlink.api.v1.encoder import Encoder
 @app.route("/api/v1/encode", methods=["POST"])
 @limiter.limit("10/second", override_defaults=False)
 def encode() -> Dict[str, str]:
+	"""Encodes a long URL to a shortened URL.
+
+	Parameters: fetched from request
+	url (str): URL which has to be shortened.
+	length (int): Length of the hash to be generated, defaults to 8.
+	expiry (datetime str isoformat): Expiration of the link, defaults to 7 days after creation.
+
+	Returns:
+	dict: Shortened URL
+	"""
+
 	encoder = Encoder(
 		url=request.args.get("url"),
 		length=request.args.get("length"),
@@ -26,6 +37,15 @@ def encode() -> Dict[str, str]:
 @app.route("/api/v1/decode", methods=["GET"])
 @limiter.limit("10/second", override_defaults=False)
 def decode() -> Dict[str, str]:
+	"""Decodes a shortened URL to its original URL.
+
+	Parameters: fetched from request
+	url (str): Short URL to be decoded.
+
+	Returns:
+	dict: Original URL
+	"""
+
 	decoder = Decoder(url=request.args.get("url"))
 
 	return decoder.get_long_url()
@@ -33,4 +53,6 @@ def decode() -> Dict[str, str]:
 
 @app.after_request
 def after_request(response):
+	"""Used for logging and analytics"""
+	# TODO logging and analytics
 	return response
